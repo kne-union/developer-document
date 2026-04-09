be1b24f19cd97da1a415a0ff638c0fb23b3664a1
Linzp <sunandmoon001@163.com>
2026-04-09 18:14:41 +0800

    升级依赖包

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --name developer-document -p 8064:8061 -d --restart=always -e IS_TEST=true ghcr.io/kne-union/developer-document
+docker run --name developer-document -p 8064:8061 -d --restart=always --env-file ./server/.env -e DB_HOST=host.docker.internal ghcr.io/kne-union/developer-document
